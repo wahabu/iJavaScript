eb8dbfa0b00b4df9ba8018ad1fa5fcd4433bccf2
wahabu <ritscg2hibo@gmail.com>
2023-04-29 13:58:17 +0900

    Soccer Team Code

---
+++ iSolution/Overview/SoccerTeam.js
@@ -0,0 +1,8 @@
+/* Soccer Team
+first game = won = 3 points
+second game = won = 3 points
+third game = won = 3 points
+forth game = draw = 1 points */
+
+// calculate and output the total points
+console.log(3 * 3 + 1 * 1);
