0920808065dba3584465bbbb6d5f40e8993f51ce
wahabu <ritscg2hibo@gmail.com>
2025-07-02 23:19:35 +0900

    Add Seasonal Message Addition program

---
+++ iPractice/SeasonalMessageAddition_S1.js
@@ -0,0 +1,7 @@
+const challengeGreeting = () => {
+  const generalMessage = "Hello! Welcome to Terrace Inn & Suites!";
+  const seasonalMessage =
+  "Need to cool off? Visit one of our three resort pools!";
+  return generalMessage + " " + seasonalMessage;
+};
+console.log(challengeGreeting());
